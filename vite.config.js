97cbd116d7ea1938ff74bdafed3ac9938577bc08
yanphayu <yanphayu@gmail.com>
2026-06-01 12:14:43 +0700

    completec version-1.1

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(),tailwindcss()],
+  build: {
+    sourcemap: false,
+    minify: 'esbuild',
+  },
 })
